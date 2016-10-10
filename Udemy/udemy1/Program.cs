c5aad8b1a541757e47f6a1b963aa92a37dcf9d4f
Diogo <diogori@gmail.com>
2016-10-10 19:03:39 -0400

    Udemy course

---
+++ Udemy/udemy1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            public void addNumbers(int myNum, int otherNum);
+            {
+            int result = myNum + otherNum;
+            Console.WriteLine(result);
+            }
+        }    
+    }
+}
